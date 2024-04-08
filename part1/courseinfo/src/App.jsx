346ee0c747f5382fa3702b13a0e92335950547f5
ARK2K <ark11.01.2000@gmail.com>
2024-04-08 16:27:23 +0530

    Exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,18 @@
 const App = () => {
   // const-definitions
   const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
@@ -11,10 +23,8 @@ const App = () => {
   );
 };
 
-const Header = ({ course }) => {
-  // const-definitions
-
-  return <p>{course}</p>;
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
 };
 
 const Content = () => {
