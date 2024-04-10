7ca7b0801304ed1860e7bf3e6b54417ebeb27b5b
ARK2K <ark11.01.2000@gmail.com>
2024-04-10 22:24:33 +0530

    Update App.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,8 +5,44 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const Click = (text) => {
+    if (text == 1) {
+      setGood(good + 1);
+    }
+    if (text == 0) {
+      setNeutral(neutral + 1);
+    }
+    if (text == 2) {
+      setBad(bad + 1);
+    }
+  };
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <button onClick={Click(1)}>Good</button>
+      <button onClick={Click(0)}>Neutral</button>
+      <button onClick={Click(2)}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {good + bad + neutral}</p>
+      <p>
+        Average: <Average val={good} sum={good + neutral + bad} />
+      </p>
+      <p>
+        Positive: <Percentage val={good} sum={good + neutral + bad} />%
+      </p>
+    </>
+  );
+};
 
-  return <div>code here</div>;
+const Average = ({ val, sum }) => {
+  //return val / sum;
+};
+
+const Percentage = ({ val, sum }) => {
+  return (val / sum) * 100;
 };
 
 export default App;
