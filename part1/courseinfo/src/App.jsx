55c0b2225e463f72f9a45e95ae7424ab687c2095
ARK2K <ark11.01.2000@gmail.com>
2024-04-10 21:49:52 +0530

    unicafe step 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,46 +1,12 @@
+import { useState } from "react";
+
 const App = () => {
-  // const-definitions
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Content = () => {
-  // const-definitions
-
-  return <p>Content</p>;
-};
-
-const Total = () => {
-  // const-definitions
-
-  return <p>Total</p>;
+  return <div>code here</div>;
 };
 
 export default App;
