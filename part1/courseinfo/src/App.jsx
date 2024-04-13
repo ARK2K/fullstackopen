65e2c7c4f7527c673aa30d445aa2b8eec7c358d7
ARK2K <ark11.01.2000@gmail.com>
2024-04-13 18:47:49 +0530

    unicafe step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,44 +5,31 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const Click = (text) => {
-    if (text == 1) {
-      setGood(good + 1);
-    }
-    if (text == 0) {
-      setNeutral(neutral + 1);
-    }
-    if (text == 2) {
-      setBad(bad + 1);
-    }
+  let all = good + neutral + bad;
+  const handleClick = () => {
+    setGood(good + 1);
+  };
+  const handleClick1 = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleClick2 = () => {
+    setBad(bad + 1);
   };
   return (
     <>
       <h1>Give Feedback</h1>
-      <button onClick={Click(1)}>Good</button>
-      <button onClick={Click(0)}>Neutral</button>
-      <button onClick={Click(2)}>Bad</button>
+      <button onClick={handleClick}>Good</button>
+      <button onClick={handleClick1}>Neutral</button>
+      <button onClick={handleClick2}>Bad</button>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-      <p>All: {good + bad + neutral}</p>
-      <p>
-        Average: <Average val={good} sum={good + neutral + bad} />
-      </p>
-      <p>
-        Positive: <Percentage val={good} sum={good + neutral + bad} />%
-      </p>
+      <p>All: {all}</p>
+      <p>Average: {(good - bad) / all}</p>
+      <p>Positive: {good / all}%</p>
     </>
   );
-};
-
-const Average = ({ val, sum }) => {
-  //return val / sum;
-};
-
-const Percentage = ({ val, sum }) => {
-  return (val / sum) * 100;
 };
 
 export default App;
