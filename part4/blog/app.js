bfc2463cc915e96a465aab07ecef16a3ac06a5a6
ARK2K <ark11.01.2000@gmail.com>
2024-05-06 22:53:19 +0530

    4.2 Blog List, step 2

---
+++ part4/blog/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+const Blog = require("./models/blog");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+  console.log(request);
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = app;
