3eb07cbf3f8396ced9572b01192b3ffdcd70bca2
ARK2K <ark11.01.2000@gmail.com>
2024-05-07 22:01:11 +0530

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,12 @@
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
+
 const dummy = (blogs) => {
   return 1;
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
