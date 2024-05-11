12e52f8ebaf5cdd409b9e97d6e331190f74d7971
ARK2K <ark11.01.2000@gmail.com>
2024-05-11 13:35:50 +0530

    4.9: Blog List Tests, step 2

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, describe } = require("node:test");
 const Blog = require("../models/blog");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -8,25 +8,37 @@ const assert = require("node:assert");
 
 const api = supertest(app);
 
-test.only("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe.only("GET /api/blogs", () => {
+  test.only("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test.only("there are two blogs", async () => {
-  const response = await api.get("/api/blogs");
+  test.only("there are two blogs", async () => {
+    const response = await api.get("/api/blogs");
 
-  assert.strictEqual(response.body.length, 2);
-});
+    assert.strictEqual(response.body.length, 2);
+  });
 
-test.only("the first blog is about React patterns", async () => {
-  const response = await api.get("/api/blogs");
+  test.only("the first blog is about React patterns", async () => {
+    const response = await api.get("/api/blogs");
 
-  const contents = response.body.map((e) => e.title);
-  // is the argument truthy
-  assert(contents.includes("React patterns"));
+    const contents = response.body.map((e) => e.title);
+    // is the argument truthy
+    assert(contents.includes("React patterns"));
+  });
+
+  test.only("returns list of blogs with id property", async () => {
+    const response = await api.get("/api/blogs").expect(200);
+    assert.ok(Array.isArray(response.body));
+
+    response.body.forEach((blog) => {
+      assert(blog.id !== undefined);
+      assert(blog._id === undefined);
+    });
+  });
 });
 
 after(async () => {
