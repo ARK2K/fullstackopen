9144fdde5dd85160d62abcb6fade060dd7fbde63
ARK2K <ark11.01.2000@gmail.com>
2024-05-27 20:45:12 +0530

    5.2 & 5.3
    5.2: Blog List Frontend, step 2 & 5.3: Blog List Frontend, step 3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,22 +5,32 @@ import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [newBlogs, setNewBlogs] = useState({ title: "", author: "", url: "" });
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
   const handleLogin = async (event) => {
     event.preventDefault();
-
     try {
       const user = await loginService.login({
         username,
         password,
       });
+
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
+      blogService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
@@ -30,6 +40,21 @@ const App = () => {
         setErrorMessage(null);
       }, 5000);
     }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+    blogService.setToken(null);
+  };
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    const blogObject = newBlogs;
+    blogService.create(blogObject).then((res) => {
+      setBlogs([...blogs, res]);
+      setNewBlogs({ title: "", author: "", url: "" });
+    });
   };
 
   const loginForm = () => (
@@ -57,14 +82,42 @@ const App = () => {
     </form>
   );
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewBlogs((newBlogs) => ({ ...newBlogs, [name]: value }));
+  };
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        <label htmlFor="title">Title:</label>{" "}
+        <input name="title" value={newBlogs.title} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="author">Author:</label>{" "}
+        <input name="author" value={newBlogs.author} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="url">URL:</label>{" "}
+        <input name="url" value={newBlogs.url} onChange={handleChange} />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  );
+
   return (
     <>
       {user === null ? (
         loginForm()
       ) : (
         <div>
-          <h2>blogs</h2>
+          <h1>blogs</h1>
           <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>logout</button>
+          <div>
+            <h2>Create new</h2>
+            {blogForm()}
+          </div>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
